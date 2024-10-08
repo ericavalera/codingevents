036ae40cba72d2ce82c9e8f5802d55185da167a5
Erica V <132383093+ericavalera@users.noreply.github.com>
2024-10-07 22:13:59 -0400

    update

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -21,21 +21,17 @@ public class EventController {
 
     @GetMapping
     public String displayAllEvents(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("Code With Pride");
-        events.add("Strange Loop");
-        events.add("Apple WWDC");
-        events.add("SpringOne Platform");
-        model.addAttribute("events", events);
+       model.addAttribute("events", events);
         return "events/index";
     }
 @GetMapping("create")
     public String renderCreateEventFrom(){
+
         return "events/create";
 }
-@PostMapping
+@PostMapping("create")
 public String createEvent(@RequestParam String eventName){
         events.add(eventName);
-        return "redirect:/event";
+        return "redirect:";
     }
 }
