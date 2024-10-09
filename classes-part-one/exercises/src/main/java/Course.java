886be8048bcedf49e97b789ad3dbf89efb64b88d
Erica V <132383093+ericavalera@users.noreply.github.com>
2024-10-09 18:30:34 -0400

    Delete classes-part-one directory

--- classes-part-one/exercises/src/main/java/Course.java
+++
@@ -1,9 +0,0 @@
-
-import java.util.ArrayList;
-
-public class Course {
- private int courseNum;
- private ArrayList <Teacher> instructor;
- private String courseName;
- private ArrayList<Student> enrolledStudents;
-}
--- classes-part-one/exercises/src/main/java/StudentPractice.java
+++
@@ -1,6 +0,0 @@
-public class StudentPractice {
-    public static void main(String[] args){
-        //instantiate your Student class below
-        Student student = new Student("Erica", 3636, 1, 4.0);
-    }
-}
