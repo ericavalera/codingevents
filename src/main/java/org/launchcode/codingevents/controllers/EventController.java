9ccf39fb8ef39b471bb8f2aac831c92a1476692e
Erica V <132383093+ericavalera@users.noreply.github.com>
2024-10-14 23:44:31 -0400

    Added Unique ID

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -50,14 +50,14 @@ public class EventController {
         }
         return "redirect:/events";
     }
-@GetMapping("edit/{eventID}")
-    public String displayEditForm(Model model, @PathVariable int eventId){
-        Event eventToEdit= EventData.getById(eventId);
+    @GetMapping("/edit/{eventId}/")
+    public String displayEditForm(Model model, @PathVariable int eventId) {
+        Event eventToEdit = EventData.getById(eventId);
         model.addAttribute("event", eventToEdit);
-        String title = "Edit Event" + eventToEdit.getName() + " (id=" + eventToEdit.getId() + ")";
-        model.addAttribute("title",title);
+        String title = "Edit Event " + eventToEdit.getName() + " (id=" + eventToEdit.getId() + ")";
+        model.addAttribute("title", title);
         return "events/edit";
-}
+    }
 
     @PostMapping("edit")
     public String processEditForm(int eventId, String name, String description) {
